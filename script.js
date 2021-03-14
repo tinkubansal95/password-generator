220e4013721533ad77ac25579d040773539a69ec
Tinku Bansal <tinkubansal21@gmail.com>
2021-03-14 20:15:29 +1100

    Added code for generate password method

--- script.js
+++ script.js
@@ -26,14 +26,40 @@ function generatePassword(){
     var UCaseChoice = window.confirm("Do you want upper case characters in your password?");
     
     // check if user have selected atleast one from lower case and upper case letters
-    if(! LCaseChoice || UCaseChoice )
+    if(! (LCaseChoice || UCaseChoice ))
     {
       window.alert("You need to choose atleast one of the characters.\nPlease try again.");
       return;
     }
 
-    var LCaseChoice = window.confirm("Do you want numeric in your password?");
-    var LCaseChoice = window.confirm("Do you want special characters in your password?");
+    // check if user want numeric in the password
+    var NumericChoice = window.confirm("Do you want numeric in your password?");
 
+    // check if user want special characters in the password
+    var SpecialCharChoice = window.confirm("Do you want special characters in your password?");
+
+    var PassString = "";
+    var FinalPassword = "";
     
+    if(LCaseChoice){
+      PassString = PassString.concat("abcdefghijklmnopqrstuvwxyz");
+    }
+    if(UCaseChoice){
+      PassString = PassString.concat("ABCDEFGHIJKLMNOPQRSTUVWXYZ");
+    }
+    if(NumericChoice)
+    {
+      PassString = PassString.concat("0123456789");
+    }
+    if(SpecialCharChoice)
+    {
+      PassString = PassString.concat("!#$%&'()*+,-./:;<=>?@[\]^_`{|}~");
+      PassString = PassString.concat('"');
+    }
+
+    //generate password
+    for(var i=0;i<= passLength; i++){
+        FinalPassword = FinalPassword.concat(PassString.charAt(Math.floor(Math.random() * Math.floor(PassString.length))));
+    }
+    return FinalPassword;
 }
